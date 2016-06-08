817dc9dfe3dc7daf4ed7599bb1e69fa5402af5a5
David Farrell <davidnmfarrell@gmail.com>
2016-06-07 22:50:22 -0400

    prep for cpan release

--- lib/URI/Encode/XS.pm
+++ lib/URI/Encode/XS.pm
@@ -1,11 +1,10 @@
 use strict;
-use warnings;
 package URI::Encode::XS;
 
 use XSLoader;
 use Exporter 5.57 'import';
 
-our $VERSION     = '0.001';
+our $VERSION     = '0.01';
 our @EXPORT_OK   = ( qw/uri_encode uri_decode/ );
 
 XSLoader::load('URI::Encode::XS', $VERSION);
