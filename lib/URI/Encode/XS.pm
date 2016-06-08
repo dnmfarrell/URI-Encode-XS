84b5f11cab47de8b752aa27054ffbd0d5dc99745
David Farrell <davidnmfarrell@gmail.com>
2016-06-07 23:13:05 -0400

    only encode when required

--- lib/URI/Encode/XS.pm
+++ lib/URI/Encode/XS.pm
@@ -4,7 +4,7 @@ package URI::Encode::XS;
 use XSLoader;
 use Exporter 5.57 'import';
 
-our $VERSION     = '0.01';
+our $VERSION     = '0.02';
 our @EXPORT_OK   = ( qw/uri_encode uri_decode/ );
 
 XSLoader::load('URI::Encode::XS', $VERSION);
