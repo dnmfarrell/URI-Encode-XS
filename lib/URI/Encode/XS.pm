f633af56b61c687ee3465e269aa0c19f6261708f
David Farrell <davidnmfarrell@gmail.com>
2016-06-08 20:59:41 -0400

    version bump

--- lib/URI/Encode/XS.pm
+++ lib/URI/Encode/XS.pm
@@ -5,7 +5,7 @@ package URI::Encode::XS;
 use XSLoader;
 use Exporter 5.57 'import';
 
-our $VERSION     = '0.05';
+our $VERSION     = '0.06';
 our @EXPORT_OK   = ( qw/uri_encode uri_decode/ );
 
 XSLoader::load('URI::Encode::XS', $VERSION);
